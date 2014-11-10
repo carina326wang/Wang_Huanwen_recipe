de917ca582c02df19def08c958996dbca813a4b6
Carina Wang <carina326wang@gmail.com>
2014-11-09 18:53:32 -0800

    booking

--- bookme.js
+++ bookme.js
@@ -2,7 +2,40 @@ var Reservation = Parse.Object.extend("Reservation");
 
 function queryReservationsForDay(day, successCallback) {
     var query = new Parse.Query(Reservation);
-    query.equalTo("date", new Date(2015, 0, +day));
+    query.equalTo("date", new Date('Jan ' + day + ', 2015 08:00 GMT+08:00'));
+    query.find({
+        success: successCallback,
+        error: function(error) {
+            alert("Error: " + error.code + " " + error.message);
+        }
+    });
+}
+
+function refreshCalendarReservations() {
+    queryAllReservations(function(reservations) {
+        for (var i = 0; i < reservations.length; i++) {
+            var reservation = reservations[i];
+
+            var day = reservation.get('date').getUTCDate();
+            var meal = reservation.get('meal');
+            var mealIndex;
+            if (meal == 'lunch') {
+                mealIndex = 0;
+            } else if (meal == 'afternoonTea') {
+                mealIndex = 1;
+            } else if (meal == 'dinner') {
+                mealIndex = 2;
+            }
+
+            var $mealImg = $('.availability[data-day=' + day + '] img').eq(mealIndex);
+            $mealImg.unbind('click');
+            $mealImg.css('opacity', 0.2);
+        }
+    });
+}
+
+function queryAllReservations(successCallback) {
+    var query = new Parse.Query(Reservation);
     query.find({
         success: successCallback,
         error: function(error) {
@@ -15,7 +48,7 @@ function saveReservation(name, day, meal, successCallback) {
     var reservation = new Reservation();
 
     reservation.set("personName", name);
-    reservation.set("date", new Date(2015, 0, +day));
+    reservation.set("date", new Date('Jan ' + day + ', 2015 08:00 GMT+08:00'));
     reservation.set("meal", meal);
 
     reservation.save(null, {
